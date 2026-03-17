994b12677ad8501cc1b5672be8b3b477da29847a
jortizel360 <jaortiz6@miuandes.cl>
2026-03-17 17:12:35 -0300

    unifique las versiones, dado a que avanzaron codigo por distintas ramas

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import csv
 
 class Tarea:
-    def __init__(self, id: str, duracion: str):
+    def __init__(self, id: str, duracion: str, categoria: str):
         self.id = id
         self.duracion = duracion
         self.categoria: str = categoria 
@@ -13,6 +13,12 @@ class Recurso:
         self.id: str = id
         self.categoria: str = categoria
 
+class Asignacion:
+    def __init__(self, id_tarea: str, id_recurso: str, inicio: int, fin: int) -> None:
+        self.id_tarea = id_tarea
+        self.id_recurso = id_recurso
+        self.inicio = inicio
+        self.fin = fin
 class GestorTareas:
     def __init__(self):
         self.tareas = []
