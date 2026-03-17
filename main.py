d8592e8a2993d9f71db7630efadcfa21efba5e17
jortizel360 <jaortiz6@miuandes.cl>
2026-03-17 13:31:10 -0300

    definimos las "tareas" como un objeto con distintas caracteristicas, precisamente el self id le apropia el nombre a la tarea, el self. duracion, su duracion y el self.categoria, su categoria pertinente

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import sys
 import csv
 
+class Tarea:
+    def __init__(self, id: str, duracion: str):
+        self.id = id
+        self.duracion = duracion
+        self.categoria: str = categoria 
+
+        
+    
